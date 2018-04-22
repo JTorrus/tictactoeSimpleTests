5417773f67d3de6fc10464ed7ffa62601301bc10
JTorrus <jtorrusgalindo@gmail.com>
2018-04-22 21:38:25 +0200

    Methods for creating a pair of players and each type of Squares implemented in advanced tests class

--- test/java/JuegoAdvancedTest.java
+++ test/java/JuegoAdvancedTest.java
@@ -12,6 +12,8 @@ public class JuegoAdvancedTest {
         assertTrue(game.isOver());
     }
 
+    // ----------------------------------------------------------------------------------------------------------------------------------------------------------
+
     private Tablero createFullBoard() {
         Tablero board = mock(Tablero.class);
         Casilla occupiedSquare = mock(Casilla.class);
@@ -19,5 +21,33 @@ public class JuegoAdvancedTest {
 
         when(board.getCasilla(any(Posicion.class))).thenReturn(occupiedSquare);
         return board;
+    }
+
+    private Player[] createPairOfPlayers() {
+        Player playerOne = mock(Player.class);
+        when(playerOne.getChip()).thenReturn('X');
+
+        Player playerTwo = mock(Player.class);
+        when(playerTwo.getChip()).thenReturn('O');
+
+        return new Player[]{playerOne, playerTwo};
+    }
+
+    private Casilla createEmptySquare() {
+        Casilla emptySquare = mock(Casilla.class);
+
+        when(emptySquare.getChip()).thenReturn(' ');
+        when(emptySquare.isOccupied()).thenReturn(false);
+
+        return emptySquare;
+    }
+
+    private Casilla createOccupiedSquare() {
+        Casilla occupiedSquare = mock(Casilla.class);
+
+        when(occupiedSquare.getChip()).thenReturn('X');
+        when(occupiedSquare.isOccupied()).thenReturn(true);
+
+        return occupiedSquare;
     }
 }
